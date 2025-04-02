4a11b4592a43912ce3689ad4ccc20252ff49560a
Aman-ali-tnc <aman.ali@thinkncode.com>
2025-04-02 10:48:40 +0000

    created new page

--- first-app/app/page.js
+++ first-app/app/page.js
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <div className="container mx-auto p-4">
     <h1 className="text-3xl font-bold mb-8">Welcome to my First Next.js project</h1>
+    <a 
+                href={`/allcountries/`}
+                className="mt-4 inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+              >
+                View all countries
+              </a>
     </div>
   );
 }
