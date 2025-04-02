024fc9bc293211959b124eba0ef8e687d56ad9d2
Aman-ali-tnc <aman.ali@thinkncode.com>
2025-04-02 10:44:26 +0000

    created new page

--- first-app/app/page.js
+++ first-app/app/page.js
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <>
-    
-    </>
+    <div className="container mx-auto p-4">
+    <h1 className="text-3xl font-bold mb-8">Welcome to my First Next.js project</h1>
+    </div>
   );
 }
